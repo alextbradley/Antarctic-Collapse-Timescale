47b918b92834aa733589894e1ad8917f0efe3ee3
Alex Bradley <alextbradley.1@gmail.com>
2023-02-13 20:50:00 +0000

    fix error in get_dimless_crev_depth
    error where might have only one root because second root close to zero not picked up

--- functions/get_dimless_crev_depth.m
+++ functions/get_dimless_crev_depth.m
@@ -6,6 +6,7 @@ db = 1e-2:1e-2:(1-1e-2);  %grid for the stress intensity algoritm
 
 
 dbc = intersections([0,1], [pp.F,pp.F], db, stress_intensity);
+dbc
 
 if isempty(dbc) %no root
     if all(stress_intensity > pp.F)
@@ -14,8 +15,14 @@ if isempty(dbc) %no root
     dimless_crev_depth = 0;
     end
 
-elseif length(dbc) == 1 %must be increasing so only one root
-    dimless_crev_depth = 1;
+elseif length(dbc) == 1 %one root. 
+    %find the point in db closest to root
+    [~,idx] = min(db - dbc);
+    if all(db(idx+1:end) > dbc) %if all points to the right larger, it must be unstable
+        dimless_crev_depth = 1;
+    else
+        dimless_crev_depth = dbc;
+    end
 elseif length(dbc) == 2
     dimless_crev_depth = max(dbc);
 end
