3538524d258a54046ccc0f1fb797e97d271705b0
Alex Bradley <alextbradley.1@gmail.com>
2023-03-16 11:25:12 +0000

    cpu updates

--- gendata/figure3/LEFM/run.sh
+++ gendata/figure3/LEFM/run.sh
@@ -6,7 +6,7 @@
 ##SBATCH --time=00:20:00         # adjust this to match the walltime of your job
 #SBATCH --nodes=1      
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=56     # adjust this if you are using parallel commands
+#SBATCH --cpus-per-task=36     # adjust this if you are using parallel commands
 
 matlab -nosplash -nodisplay -r run_get_shelf_collapse_time_HPC
 
