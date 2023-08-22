d155669192058261fb7f499900406ef071cca600
Alex Bradley <alextbradley.1@gmail.com>
2023-08-22 10:38:37 +0100

    change coefficients to shelf based in fig 4 data

--- gendata/figure4/gendata.m
+++ gendata/figure4/gendata.m
@@ -14,14 +14,16 @@ for iM = 1:length(dMs)
         shelf = shelf_names(is);
      
         % run the bisection for this shelf and this value d_m = guess;
-        s_epsxx = 1.7127e-04;
-        s_dhdt = -0.0836;     %circum Antarctic collapse co-efficients
+%         s_epsxx = 1.7127e-04; 
+%         s_dhdt = -0.0836;     %numbers based on all points around Antarctic 
+        s_epsxx =  6.8109e-04; 
+        s_dhdt = -0.4724;      %numbers based on regression of shelves (see supplementary figure D)
+
         d_m = dM;
         d_epsxx = d_m*s_epsxx;
         d_dhdt = d_m*s_dhdt;
 
         collapse_time = get_ave_shelf_collapse_time(shelf, step, d_m, d_epsxx,d_dhdt, f);
-        %collapse_time = 1;
 
 
         % directory:
