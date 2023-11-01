81099e3f591a30a2301c4ef8a73f090f278e20c5
Alex Bradley <alextbradley.1@gmail.com>
2023-11-01 17:49:09 +0000

    Update get_dimless_crev_depth_ModifiedNye.m

--- functions/get_dimless_crev_depth_ModifiedNye.m
+++ functions/get_dimless_crev_depth_ModifiedNye.m
@@ -1,6 +1,6 @@
 function dimless_crev_depth = get_dimless_crev_depth_ModifiedNye(pp, anonT)
 %return the dimensionless crevasse depth for the modified Nye case
-
+dz = 1e-2;
 %search the space
 [Rxx_nondim, ds, db] = spacesearch_ModifiedNye(anonT);
 
@@ -13,11 +13,15 @@ k = 1.17;
 
 % compute depth averaged viscosity 
 T = anonT;
+
 zz = dz:dz:(1-dz);
 B = exp(T0./T(zz) - C./(Tr - T(zz)).^k);
 Bbar= 2*sum(B)*dz;
 
 Rxx = 2*Bbar.* sign(pp.epsxx).*(abs(pp.epsxx)).^(1/n);
+rhoi = 918.0;
+rhow = 1028.0;
+g = 9.81;
 RxxIT = 0.5 * (1-rhoi/rhow)*rhoi*g*pp.H0;
 
 if Rxx > RxxIT
